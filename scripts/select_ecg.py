4d63583e7714b873b19dbddaa0700d6b9c9d4ba2
Davi <davi.nascimento777@gmail.com>
2020-05-12 22:19:29 -0300

    Separados EDF's com ECG

---
+++ scripts/select_ecg.py
@@ -0,0 +1,41 @@
+# Script para  identificação de EDF's com ECG
+
+#%% 
+import numpy as np 
+import pandas as pd 
+import os 
+
+path = os.getcwd().replace('scripts','')
+path
+# %%
+w = pd.DataFrame(
+            data=np.load(path+'data/train_seizure.npy'),
+            columns=['path','inicio','fim','tipo','freq','tempo','montage'])
+
+n = pd.DataFrame(
+            data=np.load(path+'data/train_n_seizure.npy'),
+            columns=['path','freq','tempo','montage'])
+
+# %%
+
+w_ = w['montage'].to_numpy()
+
+index_w = []
+for i in range(len(w_)):
+    for ii in range(len(w_[i])):
+        if 'EKG' in w_[i][ii]:
+            index_w.append(i)
+
+n_ = n['montage'].to_numpy()
+
+index_n = []
+for i in range(len(n_)):
+    for ii in range(len(n_[i])):
+        if 'EKG' in n_[i][ii]:
+            index_n.append(i)
+
+
+# %%
+
+np.save('ecg_train_seizure.npy',w.iloc[index_w,:].to_numpy())
+np.save('ecg_train_n_seizure.npy',n.iloc[index_n,:].to_numpy())
