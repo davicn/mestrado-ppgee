5e78344ac54cb3c4d6c95d75ef00bda268306b24
Davi <davi.nascimento777@gmail.com>
2020-05-16 10:18:00 -0300

    Script inicial para extraçã ode características

--- scripts/getfetures_emg_gnsz.py
+++ scripts/getfetures_emg_gnsz.py
@@ -2,9 +2,10 @@ import numpy as np
 import pandas as pd
 from scipy.stats import skew, kurtosis, moment
 import mne
+from os.path import expanduser
 import sys
 sys.path.append('./')
-from functions import support
+from functions.support import aux,edfArray
 
 
 c1 = pd.DataFrame(
@@ -15,5 +16,14 @@ c2 = pd.DataFrame(
     data=np.load('data/emg_train_n_seizure.npy'),
     columns=['path','freq','tempo','montage'])
 
-print(c2.head())
+HOME = expanduser('~')+'/edf/'
+
+for i in range(100):
+    raw = edfArray(HOME+c1.loc[i,'path'].replace('.tse','.edf'),'EMG')
+    v = aux(raw,np.var)
+    s = aux(raw,skew)
+    k = aux(raw,kurtosis) 
+    
+
+
 
