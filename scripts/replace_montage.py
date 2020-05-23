cafd3ac9bda5819d97ae7a936bb61e95bb7fdd86
Davi <davi.nascimento777@gmail.com>
2020-05-23 14:40:13 -0300

    Inserindo o tipo de montagem de EEG

--- scripts/replace_montage.py
+++ scripts/replace_montage.py
@@ -9,12 +9,28 @@ HOME = os.path.expanduser('~')+'/edf/'
 PATH = os.getcwd()
 
 c1 = pd.DataFrame(
-    np.load(PATH+'/data/train_seizure.npy'),
+    data=np.load(PATH+'/data/train_seizure.npy'),
     columns=['path','inicio','fim','tipo','freq','tempo','montage'])
 
 c1 = c1[(c1['freq']==256) & (c1['tipo']=='GNSZ')]
 c1.index = np.arange(len(c1))
 
-m, num = np.unique(c1['montage'],return_counts=True)
+c2 = pd.DataFrame(
+    data=np.load(PATH+'/data/train_n_seizure.npy'),
+    columns=['path','freq','tempo','montage'])
 
-print(num)
+tm1 = c1.loc[:,'path'].apply(lambda x: x.replace('train/','')[:9])
+tm2 = c2.loc[:,'path'].apply(lambda x: x.replace('train/','')[:9])
+
+# unique, cont = np.unique(tm,return_counts=True)
+# print(unique)
+# print(cont)
+# 
+# print(tm.shape)
+# print(c1.shape)
+
+c2['tipo_montage'] = tm2
+c1['tipo_montage'] = tm1
+
+np.save("train_seizure.npy",c1.to_numpy())
+np.save("train_n_seizure.npy",c2.to_numpy())
