7364e62d22c16e21b0325f77f718f05a82369c80
Davi Nascimento <davi.nascimento777@gmail.com>
2020-07-19 10:10:06 -0300

    Tabelas de treino em formato .pkl

--- scripts/teste.py
+++ scripts/teste.py
@@ -9,7 +9,7 @@ sys.path.append('./')
 
 from functions import support
 
-train = np.load('data/train_seizure.npy')
+""" train = np.load('data/train_seizure.npy')
 
 path = '/media/davi/2526467b-9f07-4ce5-9425-6d2b458567b7/home/davi/edf/'
 
@@ -22,3 +22,11 @@ info = mne.create_info(ch_names=list(signal.index),sfreq=256)
 raw = mne.io.RawArray(signal.to_numpy(),info)
 
 raw.plot(title='Data from arrays',show=True, block=True) 
+ """
+
+
+d = pd.DataFrame(
+    data=np.load('data/train_seizure.npy'),
+    columns=['path','start','end','event','freq','time','montage','type'])
+print(d)
+d.to_pickle('train_seizure.pkl')
